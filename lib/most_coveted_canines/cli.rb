022375bb7b55849b623cf08cf58019978b20c4f2
Jenna Leopold <jleopold424@gmail.com>
2017-04-05 14:06:24 +0000

    added #welcome_user to CLI class

--- lib/most_coveted_canines/cli.rb
+++ lib/most_coveted_canines/cli.rb
@@ -1,7 +1,13 @@
 class MostCovetedCanines::CLI
 
   def call
-    puts "*~*~*~*~*~* The 50 Most Popular Dog Breeds in the U.S. *~*~*~*~*~*"
+    welcome_user
+  end
+
+  def welcome_user
+    puts ""
+    puts "*~*~*~*~*~* The 50 Most Popular Dog Breeds in the United States *~*~*~*~*~*"
+    puts ""
   end
 
 end
