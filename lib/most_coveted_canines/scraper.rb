e691b0a59c6a5a40f796994b4bb31e5def4c407b
Jenna Leopold <jleopold424@gmail.com>
2017-04-06 17:25:41 +0000

    defined #scrape_ranking_list method

--- lib/most_coveted_canines/scraper.rb
+++ lib/most_coveted_canines/scraper.rb
@@ -1,14 +1,14 @@
 class MostCovetedCanines::Scraper
-  # When a dog instance is instantiated, is it initialized with any attributes?
-  # A dog instance will be initialized with @breed and @url attributes.
-  # I will need to use the @url attribute to eventually scrape an individual dog's profile page
-  # A dog instance will be initialized with a hash of keys that correspond to breed and url attributes. The hash values are the scraped data.
+  # When a dog instance is instantiated by Dog class, the instance will be initialized with a hash of keys and values
+  # The keys of each hash will be @breed and @url attributes (represented as :breed and :url symbols in hash)
+  # The corresponding values will be the scraped data
+  # Eventually, I will need to use the value of the @url property to scrape an individual dog's profile page
   def self.scrape_ranking_list
-    # the return value of this method will be an array of hashes, and each dog instance will be initialized with one of these hash elements
-    # stub out the local variable @array_of_hashes to an empty array
-    @array_of_hashes = []
+    # The return value of this method will be an array of hashes. A dog instance will be initialized with a hash element from array
+    array_of_hashes = []
+    # the #open method returns the HTML content of the web page specified in the argument
+    # the Nokogiri::HTML method converts this into a giant HTML nested node string
+    # the local variable node_set will store this giant nested node HTML string of ranking list web page
+    node_set = Nokogiri::HTML(open(http://www.akc.org/content/news/articles/most-popular-dog-breeds-full-ranking-list/))
   end
-
-
-
 end
