19530a712d3f06e37790d54a0829b144e9fefa67
Jenna Leopold <jleopold424@gmail.com>
2017-04-06 18:00:05 +0000

    required open-uri, nokogiri, pry and scraper class file in environment file

--- lib/most_coveted_canines.rb
+++ lib/most_coveted_canines.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative './most_coveted_canines/version'
 require_relative './most_coveted_canines/cli'
 require_relative './most_coveted_canines/dog'
+require_relative './most_coveted_canines/scraper'
