b21c760043d0427fa23958712081ef41ac24e3a1
Jenna Leopold <jleopold424@gmail.com>
2017-04-06 23:38:59 +0000

    added #create_canine_collection(array_of_hashes) to Dog class

--- lib/most_coveted_canines/dog.rb
+++ lib/most_coveted_canines/dog.rb
@@ -1,6 +1,12 @@
+require_relative './scraper.rb'
 class MostCovetedCanines::Dog
   attr_accessor :breed, :url, :group, :personality, :year_recognized, :rank, :grooming, :exercise, :fun_fact, :appearance
   @@all = []
+
+  def self.create_canine_collection(array_of_hashes) # argument is return value of Scraper#scrape_ranking_list
+    array_of_hashes.each {|canine_hash| self.new(canine_hash)}
+    binding.pry
+  end
 
   def self.all
     # returns array of all dog instances
