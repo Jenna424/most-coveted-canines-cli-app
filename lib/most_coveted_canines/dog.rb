d54030969864fc6642cb97e5f45a1bdbab9778a0
Jenna Leopold <jleopold424@gmail.com>
2017-04-06 23:42:31 +0000

    added #initialize method to initialize dog instance with @breed and @url

--- lib/most_coveted_canines/dog.rb
+++ lib/most_coveted_canines/dog.rb
@@ -3,6 +3,11 @@ class MostCovetedCanines::Dog
   attr_accessor :breed, :url, :group, :personality, :year_recognized, :rank, :grooming, :exercise, :fun_fact, :appearance
   @@all = []
 
+  def initialize(canine_hash)
+    canine_hash.each {|k,v| self.send("#{k}=", v)}
+  end
+    
+
   def self.create_canine_collection(array_of_hashes) # argument is return value of Scraper#scrape_ranking_list
     array_of_hashes.each {|canine_hash| self.new(canine_hash)}
     binding.pry
