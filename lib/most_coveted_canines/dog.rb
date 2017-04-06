c8658b968e6cc767fa37f7ae269f22bcfb18dc3e
Jenna Leopold <jleopold424@gmail.com>
2017-04-06 23:50:21 +0000

    defined @@puppies, create class reader for @@puppies and pushed new dog instances into @@puppies

--- lib/most_coveted_canines/dog.rb
+++ lib/most_coveted_canines/dog.rb
@@ -2,11 +2,19 @@ require_relative './scraper.rb'
 class MostCovetedCanines::Dog
   attr_accessor :breed, :url, :group, :personality, :year_recognized, :rank, :grooming, :exercise, :fun_fact, :appearance
   @@all = []
+  @@puppies = []
+
+  def self.puppies #class reader
+    @@puppies
+  end
+
+  # when does a dog instance get saved?
 
   def initialize(canine_hash)
     canine_hash.each {|k,v| self.send("#{k}=", v)}
+    @@puppies << self
   end
-    
+
 
   def self.create_canine_collection(array_of_hashes) # argument is return value of Scraper#scrape_ranking_list
     array_of_hashes.each {|canine_hash| self.new(canine_hash)}
