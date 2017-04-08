1f40530ca0ea766ae31d4ebc00eea20ff593a77b
Jenna Leopold <jleopold424@gmail.com>
2017-04-08 14:54:58 +0000

    removed case statement from #reveal_range(first_in_range)

--- lib/most_coveted_canines/cli.rb
+++ lib/most_coveted_canines/cli.rb
@@ -72,70 +72,15 @@ class MostCovetedCanines::CLI
       unleash_dogs
     end
   end
-
+# In my Dog class, my @@all array stores all dog instances (that have all attributes assigned)
+# I'm now going to replace my hard-coded string data in the #reveal_range method with my actual dog instances the Dog class created
+# First, I'm going to remove my case statement in #reveal_range(first_in_range) that contains the hard-coded strings
   def reveal_range(first_in_range)
     @all_dogs = MostCovetedCanines::Dog.all
     puts "Dogs Ranked from Numbers #{first_in_range} through #{first_in_range + 9}:"
     # ultimately, this method should first get array of dog instances at specific array indices
     # then iterate through that array of dog instances and for each dog instance, print rank number followed by breed attribute
     # There are 10 numbers in each range, so the first number in the range + 9 is the last number in the range
-    case first_in_range
-    when 1
-      puts "1. #{@all_dogs.first.breed}"
-      puts "2. #{@all_dogs[1].breed}"
-      puts "3. Retrievers (Golden)"
-      puts "4. Bulldogs"
-      puts "5. Beagles"
-      puts "6. French Bulldogs"
-      puts "7. Poodles"
-      puts "8. Rottweilers"
-      puts "9. Yorkshire Terriers"
-      puts "10. Boxers"
-    when 11
-      puts "11. Pointers (German Shorthaired)"
-      puts "12. Siberian Huskies"
-      puts "13. Dachshunds"
-      puts "14. Great Danes"
-      puts "15. Doberman Pinschers"
-      puts "16. Australian Shepherds"
-      puts "17. Miniature Schnauzers"
-      puts "18. Pembroke Welsh Corgis"
-      puts "19. Cavalier King Charles Spaniels"
-      puts "20. Shih Tzu"
-    when 21
-      puts "21. Boston Terriers"
-      puts "22. Pomeranians"
-      puts "23. Havanese"
-      puts "24. Shetland Sheepdogs"
-      puts "25. Brittanys"
-      puts "26. Spaniels (English Springer)"
-      puts "27. Bernese Mountain Dogs"
-      puts "28. Mastiffs"
-      puts "29. Spaniels (Cocker)"
-      puts "30. Chihuahuas"
-    when 31
-      puts "31. Vizslas"
-      puts "32. Pugs"
-      puts "33. Maltese"
-      puts "34. Weimaraners"
-      puts "35. Newfoundlands"
-      puts "36. Miniature American Shepherds"
-      puts "37. Collies"
-      puts "38. Border Collies"
-      puts "39. Basset Hounds"
-      puts "40. Cane Corso"
-    when 41
-      puts "41. West Highland White Terriers"
-      puts "42. Rhodesian Ridgebacks"
-      puts "43. Retrievers (Chesapeake Bay)"
-      puts "44. Shiba Inu"
-      puts "45. Bichons Frises"
-      puts "46. Akitas"
-      puts "47. Belgian Malinois"
-      puts "48. Bullmastiffs"
-      puts "49. St. Bernards"
-      puts "50. Soft Coated Wheaten Terriers"
-    end
   end
 
   def demand_dog
